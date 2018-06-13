42fbfdb4adcce0c93d0918d1c586351fa2136a9f
khangvtran <vinhkhangtran09@gmail.com>
2018-06-12 18:26:34 -0700

    initial commit

---
+++ problem1.py
@@ -0,0 +1,16 @@
+
+"""
+Khang Vinh Tran
+Spotify Assessment 06/12/2018
+Language: Python 3
+
+Question 1 -- sortByStrings(s,t): Sort the letters in the string s by the order they occur in the string t. 
+You can assume t will not have repetitive characters.
+For s = "weather" and t = "therapyw", the output should be sortByString(s, t) = "theeraw".
+For s = "good" and t = "odg", the output should be sortByString(s, t) = "oodg".
+"""
+
+
+
+if __name__ == "__main__":
+    print("Hello World")
